7853b5defc49ac0a7ea4dbbe7e28b6a44b4c00b4
KSVarun <ksvarun001@gmail.com>
2019-06-27 23:35:25 +0530

    reg ex

--- rename_files.py
+++ rename_files.py
@@ -7,15 +7,15 @@ for file in os.listdir(dir_path):
     """file_path = os.path.join(dir_path, file)
     with open(file_path, 'r') as file:
         print(file.read())"""
-    inside_dir_path = dir_path+file+"/"
+    print(file)
+    """inside_dir_path = dir_path+file+"/"
+    # print(inside_dir_path)
     for file_inside in os.listdir(inside_dir_path):
-        """src=inside_dir_path+file_inside
-        str_i = str(i)
-        dest=inside_dir_path+str_i+file_inside
-        os.rename(src,dest)
-        i=i+1"""
-        searched_string = re.search('[^\w+].mp4$', file_inside)
-        print(searched_string)
+        
+        # print(file_inside)
+        searched_string = re.search(
+            '\d+\.(\w*\s)*\w*.mp4$', file_inside)
+        print(searched_string)"""
 
     # for testing
     # print(file)
