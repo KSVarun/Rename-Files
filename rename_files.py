044a6de25ca82671489bdfce14479658a54d15aa
KSVarun <ksvarun001@gmail.com>
2019-06-29 22:20:33 +0530

    beautiful code

--- rename_files.py
+++ rename_files.py
@@ -1,21 +1,34 @@
 import os
 import re
 
+
+def sorted_alphanumeric(data):
+    def convert(text): return int(text) if text.isdigit() else text.lower()
+    def alphanum_key(key): return [convert(c)
+                                   for c in re.split('([0-9]+)', key)]
+    return sorted(data, key=alphanum_key)
+
+
 dir_path = "/Users/idks/Downloads/MastertheCodingInterviewDataStructures+AlgorithmsCOPY/"
 i = 1
-for file in os.listdir(dir_path):
+path = sorted_alphanumeric(os.listdir(dir_path))
+for x in path:
+    if(x == '.DS_Store'):
+        path.remove(x)
+
+for file in path:
     """file_path = os.path.join(dir_path, file)
     with open(file_path, 'r') as file:
         print(file.read())"""
-    print(file)
-    """inside_dir_path = dir_path+file+"/"
-    # print(inside_dir_path)
-    for file_inside in os.listdir(inside_dir_path):
-        
-        # print(file_inside)
-        searched_string = re.search(
-            '\d+\.(\w*\s)*\w*.mp4$', file_inside)
-        print(searched_string)"""
+    # if(file != '.DS_STORE'):
 
-    # for testing
+    inside_dir_path = dir_path+file+"/"
     # print(file)
+    path2 = sorted_alphanumeric(os.listdir(inside_dir_path))
+    mpfiles = '\d+\.(\w*\s)*\w*.mp4$'
+    # print(path2)
+    for file_inside in path2:
+        pattern = re.compile(mpfiles)
+        matches = pattern.finditer(file_inside)
+        for match in matches:
+            print(match)
